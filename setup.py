2b4380e81254b5c58b07669118375c4feac4ae55
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-09-16 10:32:52 -0300

    CI changes

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def run_conan(reference, reference_fallback):
         # c.install(reference, verify=None, manifests=None)
         c.install(reference, verify=None, manifests_interactive=None, manifests=None)
         print('11 876128376128371263876128376128371263876128376128371263876128376128371263876128376128371263')
-        pepe = find('nodecint.h', os.getcwd())
+        pepe = find('capi.h', os.getcwd())
         print(pepe)
         print('11 876128376128371263876128376128371263876128376128371263876128376128371263876128376128371263')
 
@@ -67,28 +67,16 @@ def run_conan(reference, reference_fallback):
 
         c.install(reference_fallback, verify=None, manifests_interactive=None, manifests=None)
         print('22 876128376128371263876128376128371263876128376128371263876128376128371263876128376128371263')
-        pepe = find('nodecint.h', os.getcwd())
+        pepe = find('capi.h', os.getcwd())
         print(pepe)
         shutil.move('./deps/', '..')
-        pepe = find('nodecint.h', os.getcwd())
+        pepe = find('capi.h', os.getcwd())
         print(pepe)
         print('22 876128376128371263876128376128371263876128376128371263876128376128371263876128376128371263')
 
     print('run_conan - END')
     replace_boost_lib_names_on_windows('../deps/lib')
 
-# line = 'libboost_system-vc140-mt-1_64.lib'
-
-# # line = re.sub(r"</?\[\d+>", "", line)
-
-# # libboost_\w+-vc\d\d\d-mt-\d_\d\d
-# line = re.sub(r"-\d_\d\d", "", line)
-# line = re.sub(r"-vc\d\d\d-mt", "", line)
-
-# print(line)
-
 
 if __name__ == '__main__':
     run_conan('.', '..')
-
-    # shutil.move('./deps/', '..')
