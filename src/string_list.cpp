8864114314f2167b04e0a53ff233f9c25f502685
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-09-23 17:32:36 -0300

    improves several classes

---
+++ src/string_list.cpp
@@ -0,0 +1,89 @@
+// Copyright (c) 2016-2020 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <node.h>
+
+#include <kth/capi/string_list.h>
+#include <kth/js-native/string_list.hpp>
+#include <kth/js-native/helper.hpp>
+
+namespace kth::js_native {
+
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Global;
+
+using v8::Object;
+using v8::String;
+using v8::Boolean;
+using v8::Value;
+using v8::External;
+using v8::Exception;
+using v8::Number;
+using v8::Persistent;
+using v8::Function;
+using v8::Uint8Array;
+using v8::ArrayBuffer;
+
+
+void core_string_list_construct(v8::FunctionCallbackInfo<v8::Value> const& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    if (args.Length() != 0) {
+        throw_exception(isolate, "Wrong number of arguments");
+        return;
+    }
+
+    kth_string_list_t res = kth_core_string_list_construct();
+    args.GetReturnValue().Set(External::New(isolate, res));
+}
+
+void core_string_list_destruct(v8::FunctionCallbackInfo<v8::Value> const& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    if (args.Length() != 1) {
+        throw_exception(isolate, "Wrong number of arguments");
+        return;
+    }
+
+    if ( ! args[0]->IsExternal()) {
+        throw_exception(isolate, "Wrong arguments");
+        return;
+    }
+
+    void* vptr = v8::External::Cast(*args[0])->Value();
+    kth_string_list_t string_list = (kth_string_list_t)vptr;
+
+    kth_core_string_list_destruct(string_list);
+}
+
+void core_string_list_push_back(v8::FunctionCallbackInfo<v8::Value> const& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    if (args.Length() != 2) {
+        throw_exception(isolate, "Wrong number of arguments");
+        return;
+    }
+
+    if ( ! args[0]->IsExternal()) {
+        throw_exception(isolate, "Wrong arguments");
+        return;
+    }
+
+    if ( ! args[1]->IsString()) {
+        throw_exception(isolate, "Wrong arguments");
+        return;
+    }
+
+    void* vptr = v8::External::Cast(*args[0])->Value();
+    kth_string_list_t string_list = (kth_string_list_t)vptr;
+    v8::String::Utf8Value str(isolate, args[0]->ToString(isolate->GetCurrentContext()).ToLocalChecked());
+    kth_core_string_list_push_back(string_list, *str);
+}
+
+}  // namespace kth::js_native
