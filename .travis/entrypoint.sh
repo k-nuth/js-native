330a7fa03bd9dfad78ddff88f7663f0c91f5b642
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-18 20:06:39 -0300

    Github Actions fix 7

--- .travis/entrypoint.sh
+++ .travis/entrypoint.sh
@@ -1,39 +1,10 @@
 #!/bin/bash
 
-echo "Fer 0"
-
-
-echo "Fer 1"
-
-# sudo apt-get install -y curl
-
-echo "Fer 2"
 
 node_setup="http://deb.nodesource.com/setup_$NODEJS_VERSION.x"
-
-# wget -qO- http://deb.nodesource.com/setup_8.x | sudo -E bash -
 wget -qO- $node_setup | sudo -E bash -
-
-echo "Fer 3"
-
 sudo apt-get install -y nodejs
-
-echo "Fer 4"
-
-# sudo apt-get install -y  nodejs
-# sudo apt-get install -y  nodejs-legacy
-
-
-echo "Fer 5"
-
-# sudo apt-get install -y  npm
-
-echo "Fer 6"
-
 sudo -H npm install npm@latest -g
-
-echo "Fer 7"
-
 
 sudo -H npm install -g node-gyp
 sudo -H npm install -g node-pre-gyp
@@ -41,9 +12,6 @@ sudo -H npm install -g node-pre-gyp
 sudo -H npm install -g node-pre-gyp-github
 
 # npm cache clean
-
-
-
 # sudo pip install --upgrade conan_package_tools
 # pip install --upgrade --user conan_package_tools
 # pip install conan --upgrade --user
@@ -52,19 +20,10 @@ conan user
 
 
 
-echo "Fer 7A"
-
 # npm set init.author.name "Fernando Pelliccioni"
-
-# echo "Fer 7B"
-
 # npm set init.author.email "fpelliccioni@gmail.com"
-
-# echo "Fer 7C"
-
 # npm set init.author.url "http://kth.cash"
 
-# echo "Fer 7D"
 
 # npm adduser <<!
 # $NPM_USERNAME
