5b9ff841ff9cb154b733a0f62eaf858a08416687
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-21 21:16:19 -0300

    better way to invoke pip install

--- install.py
+++ install.py
@@ -2,16 +2,20 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-import pip
+# import pip
 import shutil
 import os
 import re
 import sys
 from sys import platform
 import importlib
+import subprocess
+
+# def install(package):
+#     pip.main(['install', package])
 
 def install(package):
-    pip.main(['install', package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
@@ -49,61 +53,6 @@ def get_conan():
     except ImportError:
         # print("Conan is not installed *****************************")
         return None
-
-# def run_conan(reference, reference_fallback):
-#     # New API in Conan 0.28
-#     conan_api = get_conan()
-#     if conan_api == None:
-#         sys.exit('Conan is not installed')
-#         return False
-
-#     Conan = conan_api.Conan
-#     c, _, _ = Conan.factory()
-
-#     print(reference)
-#     print(reference_fallback)
-
-#     try:
-#         # c.remote_add(remote, url, verify_ssl, args.insert)
-#         c.remote_add('kth', 'https://api.bintray.com/conan/k-nuth/kth')
-#     except:
-#         print ("Conan Remote exists, ignoring exception.")
-
-#     print('platform --------------------------')
-#     print(platform)
-#     print('platform --------------------------')
-
-
-#     try:
-#         win_setts = ["compiler.runtime=MT"]
-#         if platform == "win32":
-#             c.install(reference, verify=None, manifests_interactive=None, manifests=None, settings=win_setts)
-#         else:
-#             # c.install(reference, verify=None, manifests=None)
-#             c.install(reference, verify=None, manifests_interactive=None, manifests=None)
-
-#         pepe = find('capi.h', os.getcwd())
-#         print(pepe)
-
-#     except:
-#         print('EXCEPTION --------------------------')
-#         print('platform --------------------------')
-#         print(platform)
-#         print('platform --------------------------')
-
-#         if platform == "win32":
-#             c.install(reference_fallback, verify=None, manifests_interactive=None, manifests=None, settings=win_setts)
-#         else:
-#             c.install(reference_fallback, verify=None, manifests_interactive=None, manifests=None)
-
-#         pepe = find('capi.h', os.getcwd())
-#         print(pepe)
-#         shutil.move('./deps/', '..')
-#         pepe = find('capi.h', os.getcwd())
-#         print(pepe)
-
-#     print('run_conan - END')
-#     replace_boost_lib_names_on_windows('../deps/lib')
 
 def run_conan(reference):
     # New API in Conan 0.28
