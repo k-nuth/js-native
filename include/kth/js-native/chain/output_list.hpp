3b23f8d90536fdd730be2df550a5ab068af2b7be
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-02 09:27:50 -0300

    Fixes OutputList API

--- include/kth/js-native/chain/output_list.hpp
+++ include/kth/js-native/chain/output_list.hpp
@@ -9,7 +9,9 @@
 
 namespace kth::js_native {
 
+void chain_output_list_construct_default(v8::FunctionCallbackInfo<v8::Value> const& args);
 void chain_output_list_push_back(v8::FunctionCallbackInfo<v8::Value> const& args);
+void chain_output_list_destruct(v8::FunctionCallbackInfo<v8::Value> const& args);
 void chain_output_list_count(v8::FunctionCallbackInfo<v8::Value> const& args);
 void chain_output_list_nth(v8::FunctionCallbackInfo<v8::Value> const& args);
 
