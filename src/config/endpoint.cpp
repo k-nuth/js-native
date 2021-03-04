5fdab44c88249eaca7611b6bd33ec963f0abff8d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-04 14:58:04 -0300

    add endpoint functions

---
+++ src/config/endpoint.cpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <node.h>
+
+#include <kth/capi/config/endpoint.h>
+
+#include <kth/js-native/config/endpoint.hpp>
+#include <kth/js-native/helper.hpp>
+
+namespace kth::js_native {
+
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Global;
+
+using v8::Object;
+using v8::String;
+using v8::Boolean;
+using v8::Value;
+using v8::External;
+using v8::Exception;
+using v8::Number;
+using v8::Persistent;
+using v8::Function;
+using v8::Uint8Array;
+using v8::ArrayBuffer;
+
+void config_endpoint_allocate_n(v8::FunctionCallbackInfo<v8::Value> const& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    if (args.Length() != 1) {
+        throw_exception(isolate, "Wrong number of arguments. config_endpoint_allocate_n function requires 1 arguments.");
+        return;
+    }
+
+    if ( ! args[0]->IsNumber()) {
+        throw_exception(isolate, "Wrong argument type for argument n (#1). Required to be IsNumber.");
+        return;
+    }
+
+    kth_size_t n = args[0]->IntegerValue(isolate->GetCurrentContext()).ToChecked();
+
+    kth_endpoint* res = kth_config_endpoint_allocate_n(n);
+    args.GetReturnValue().Set(External::New(isolate, res));
+}
+
+}  // namespace kth::js_native
