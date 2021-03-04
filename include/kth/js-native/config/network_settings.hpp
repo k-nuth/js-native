969619c584424d9035c70dd7cf625f7c335377ab
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-04 14:55:41 -0300

    add network settings header file

---
+++ include/kth/js-native/config/network_settings.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef KTH_JS_CONFIG_NETWORK_SETTINGS_HPP_
+#define KTH_JS_CONFIG_NETWORK_SETTINGS_HPP_
+
+#include <node.h>
+
+namespace kth::js_native {
+
+void config_network_settings_default(v8::FunctionCallbackInfo<v8::Value> const& args);
+
+}  // namespace kth::js_native
+
+#endif //KTH_JS_CONFIG_NETWORK_SETTINGS_HPP_
