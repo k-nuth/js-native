0a82c7932d69625aabf23c6030845dc243f2344d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-06 18:17:57 -0300

    convert network settings from C to JS

--- src/config/network_settings.cpp
+++ src/config/network_settings.cpp
@@ -18,7 +18,6 @@ using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
 using v8::Global;
-
 using v8::Object;
 using v8::String;
 using v8::Boolean;
@@ -30,6 +29,119 @@ using v8::Persistent;
 using v8::Function;
 using v8::Uint8Array;
 using v8::ArrayBuffer;
+
+
+// typedef struct {
+//     char* ip;
+//     uint16_t ;
+// } kth_authority;
+
+// typedef struct {
+//     char* scheme;
+//     char* host;
+//     uint16_t port;
+// } kth_endpoint;
+
+v8::Local<v8::Object> config_endpoint_to_js(Isolate* isolate, kth_endpoint const& endpoint) {
+    auto ctx = isolate->GetCurrentContext();
+    auto res = v8::Object::New(isolate);
+    auto setr = res->Set(ctx, to_string(isolate, "scheme"), to_string(isolate, endpoint.scheme));
+    setr = res->Set(ctx, to_string(isolate, "host"), to_string(isolate, endpoint.host));
+    setr = res->Set(ctx, to_string(isolate, "port"), Number::New(isolate, endpoint.port));
+    return res;
+}
+
+v8::Local<v8::Array> config_endpoints_to_js(Isolate* isolate, kth_endpoint* endpoint, size_t n) {
+    auto ctx = isolate->GetCurrentContext();
+    v8::Local<v8::Array> jsArr = Nan::New<v8::Array>(n);
+    for (size_t i = 0; i < jsArr->Length(); ++i) {
+        auto elem = config_endpoint_to_js(isolate, *endpoint);
+        jsArr->Set(ctx, i, elem);
+        ++endpoint;
+    }
+    return jsArr;
+}
+
+v8::Local<v8::Object> config_authority_to_js(Isolate* isolate, kth_authority const& authority) {
+    auto ctx = isolate->GetCurrentContext();
+    auto res = v8::Object::New(isolate);
+    auto setr = res->Set(ctx, to_string(isolate, "ip"), to_string(isolate, authority.ip));
+    setr = res->Set(ctx, to_string(isolate, "port"), Number::New(isolate, authority.port));
+    return res;
+}
+
+v8::Local<v8::Array> config_authorities_to_js(Isolate* isolate, kth_authority* authority, size_t n) {
+    auto ctx = isolate->GetCurrentContext();
+    v8::Local<v8::Array> jsArr = Nan::New<v8::Array>(n);
+    for (size_t i = 0; i < jsArr->Length(); ++i) {
+        auto elem = config_authority_to_js(isolate, *authority);
+        jsArr->Set(ctx, i, elem);
+        ++authority;
+    }
+    return jsArr;
+}
+
+v8::Local<v8::Array> config_strings_to_js(Isolate* isolate, char** strs, size_t n) {
+    auto ctx = isolate->GetCurrentContext();
+    v8::Local<v8::Array> jsArr = Nan::New<v8::Array>(n);
+    for (size_t i = 0; i < jsArr->Length(); ++i) {
+        auto elem = to_string(isolate, *strs);
+        jsArr->Set(ctx, i, elem);
+        ++strs;
+    }
+    return jsArr;
+}
+
+v8::Local<v8::Object> config_network_settings_to_js(Isolate* isolate, kth_network_settings const& setts) {
+    auto ctx = isolate->GetCurrentContext();
+    auto res = v8::Object::New(isolate);
+    auto setr = res->Set(ctx, to_string(isolate, "threads"), Number::New(isolate, setts.threads));
+    setr = res->Set(ctx, to_string(isolate, "protocol_maximum"), Number::New(isolate, setts.protocol_maximum));
+    setr = res->Set(ctx, to_string(isolate, "protocol_minimum"), Number::New(isolate, setts.protocol_minimum));
+    setr = res->Set(ctx, to_string(isolate, "services"), Number::New(isolate, setts.services));
+    setr = res->Set(ctx, to_string(isolate, "invalid_services"), Number::New(isolate, setts.invalid_services));
+    setr = res->Set(ctx, to_string(isolate, "relay_transactions"), Boolean::New(isolate, setts.relay_transactions != 0));
+    setr = res->Set(ctx, to_string(isolate, "validate_checksum"), Boolean::New(isolate, setts.validate_checksum != 0));
+
+    setr = res->Set(ctx, to_string(isolate, "identifier"), Number::New(isolate, setts.identifier));
+    setr = res->Set(ctx, to_string(isolate, "inbound_port"), Number::New(isolate, setts.inbound_port));
+    setr = res->Set(ctx, to_string(isolate, "inbound_connections"), Number::New(isolate, setts.inbound_connections));
+    setr = res->Set(ctx, to_string(isolate, "outbound_connections"), Number::New(isolate, setts.outbound_connections));
+    setr = res->Set(ctx, to_string(isolate, "manual_attempt_limit"), Number::New(isolate, setts.manual_attempt_limit));
+    setr = res->Set(ctx, to_string(isolate, "connect_batch_size"), Number::New(isolate, setts.connect_batch_size));
+    setr = res->Set(ctx, to_string(isolate, "connect_timeout_seconds"), Number::New(isolate, setts.connect_timeout_seconds));
+    setr = res->Set(ctx, to_string(isolate, "channel_handshake_seconds"), Number::New(isolate, setts.channel_handshake_seconds));
+    setr = res->Set(ctx, to_string(isolate, "channel_heartbeat_minutes"), Number::New(isolate, setts.channel_heartbeat_minutes));
+    setr = res->Set(ctx, to_string(isolate, "channel_inactivity_minutes"), Number::New(isolate, setts.channel_inactivity_minutes));
+    setr = res->Set(ctx, to_string(isolate, "channel_expiration_minutes"), Number::New(isolate, setts.channel_expiration_minutes));
+    setr = res->Set(ctx, to_string(isolate, "channel_germination_seconds"), Number::New(isolate, setts.channel_germination_seconds));
+    setr = res->Set(ctx, to_string(isolate, "host_pool_capacity"), Number::New(isolate, setts.host_pool_capacity));
+
+    setr = res->Set(ctx, to_string(isolate, "hosts_file"), to_string(isolate, setts.hosts_file));
+    setr = res->Set(ctx, to_string(isolate, "self"), config_authority_to_js(isolate, setts.self));
+    setr = res->Set(ctx, to_string(isolate, "blacklist"), config_authorities_to_js(isolate, setts.blacklists, setts.blacklist_count));
+    setr = res->Set(ctx, to_string(isolate, "peers"), config_endpoints_to_js(isolate, setts.peers, setts.peer_count));
+    setr = res->Set(ctx, to_string(isolate, "seeds"), config_endpoints_to_js(isolate, setts.seeds, setts.seed_count));
+
+    setr = res->Set(ctx, to_string(isolate, "debug_file"), to_string(isolate, setts.debug_file));
+    setr = res->Set(ctx, to_string(isolate, "error_file"), to_string(isolate, setts.error_file));
+    setr = res->Set(ctx, to_string(isolate, "archive_directory"), to_string(isolate, setts.archive_directory));
+
+    setr = res->Set(ctx, to_string(isolate, "rotation_size"), Number::New(isolate, setts.rotation_size));
+    setr = res->Set(ctx, to_string(isolate, "minimum_free_space"), Number::New(isolate, setts.minimum_free_space));
+    setr = res->Set(ctx, to_string(isolate, "maximum_archive_size"), Number::New(isolate, setts.maximum_archive_size));
+    setr = res->Set(ctx, to_string(isolate, "maximum_archive_files"), Number::New(isolate, setts.maximum_archive_files));
+
+    setr = res->Set(ctx, to_string(isolate, "statistics_server"), config_authority_to_js(isolate, setts.statistics_server));
+
+    setr = res->Set(ctx, to_string(isolate, "verbose"), Boolean::New(isolate, setts.verbose != 0));
+    setr = res->Set(ctx, to_string(isolate, "use_ipv6"), Boolean::New(isolate, setts.use_ipv6 != 0));
+
+    setr = res->Set(ctx, to_string(isolate, "user_agent_blacklist"), config_strings_to_js(isolate, setts.user_agent_blacklist, setts.user_agent_blacklist_count));
+
+    return res;
+}
+
 
 void config_network_settings_default(v8::FunctionCallbackInfo<v8::Value> const& args) {
     Isolate* isolate = args.GetIsolate();
@@ -45,9 +157,8 @@ void config_network_settings_default(v8::FunctionCallbackInfo<v8::Value> const& 
     }
 
     kth_network_t net = to_kth_network_t(isolate, args[0]);
-
     kth_network_settings res = kth_config_network_settings_default(net);
-    args.GetReturnValue().Set(External::New(isolate, res));
+    args.GetReturnValue().Set(config_network_settings_to_js(isolate, res));
 }
 
 }  // namespace kth::js_native
