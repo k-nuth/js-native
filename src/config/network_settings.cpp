588d6c6702bdb7b5bb6b5c18cdbe37184fecf44c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-06 20:39:27 -0300

    fix network settings names

--- src/config/network_settings.cpp
+++ src/config/network_settings.cpp
@@ -30,18 +30,6 @@ using v8::Function;
 using v8::Uint8Array;
 using v8::ArrayBuffer;
 
-
-// typedef struct {
-//     char* ip;
-//     uint16_t ;
-// } kth_authority;
-
-// typedef struct {
-//     char* scheme;
-//     char* host;
-//     uint16_t port;
-// } kth_endpoint;
-
 v8::Local<v8::Object> config_endpoint_to_js(Isolate* isolate, kth_endpoint const& endpoint) {
     auto ctx = isolate->GetCurrentContext();
     auto res = v8::Object::New(isolate);
@@ -96,52 +84,51 @@ v8::Local<v8::Object> config_network_settings_to_js(Isolate* isolate, kth_networ
     auto ctx = isolate->GetCurrentContext();
     auto res = v8::Object::New(isolate);
     auto setr = res->Set(ctx, to_string(isolate, "threads"), Number::New(isolate, setts.threads));
-    setr = res->Set(ctx, to_string(isolate, "protocol_maximum"), Number::New(isolate, setts.protocol_maximum));
-    setr = res->Set(ctx, to_string(isolate, "protocol_minimum"), Number::New(isolate, setts.protocol_minimum));
+    setr = res->Set(ctx, to_string(isolate, "protocolMaximum"), Number::New(isolate, setts.protocol_maximum));
+    setr = res->Set(ctx, to_string(isolate, "protocolMinimum"), Number::New(isolate, setts.protocol_minimum));
     setr = res->Set(ctx, to_string(isolate, "services"), Number::New(isolate, setts.services));
-    setr = res->Set(ctx, to_string(isolate, "invalid_services"), Number::New(isolate, setts.invalid_services));
-    setr = res->Set(ctx, to_string(isolate, "relay_transactions"), Boolean::New(isolate, setts.relay_transactions != 0));
-    setr = res->Set(ctx, to_string(isolate, "validate_checksum"), Boolean::New(isolate, setts.validate_checksum != 0));
+    setr = res->Set(ctx, to_string(isolate, "invalidServices"), Number::New(isolate, setts.invalid_services));
+    setr = res->Set(ctx, to_string(isolate, "relayTransactions"), Boolean::New(isolate, setts.relay_transactions != 0));
+    setr = res->Set(ctx, to_string(isolate, "validateChecksum"), Boolean::New(isolate, setts.validate_checksum != 0));
 
     setr = res->Set(ctx, to_string(isolate, "identifier"), Number::New(isolate, setts.identifier));
-    setr = res->Set(ctx, to_string(isolate, "inbound_port"), Number::New(isolate, setts.inbound_port));
-    setr = res->Set(ctx, to_string(isolate, "inbound_connections"), Number::New(isolate, setts.inbound_connections));
-    setr = res->Set(ctx, to_string(isolate, "outbound_connections"), Number::New(isolate, setts.outbound_connections));
-    setr = res->Set(ctx, to_string(isolate, "manual_attempt_limit"), Number::New(isolate, setts.manual_attempt_limit));
-    setr = res->Set(ctx, to_string(isolate, "connect_batch_size"), Number::New(isolate, setts.connect_batch_size));
-    setr = res->Set(ctx, to_string(isolate, "connect_timeout_seconds"), Number::New(isolate, setts.connect_timeout_seconds));
-    setr = res->Set(ctx, to_string(isolate, "channel_handshake_seconds"), Number::New(isolate, setts.channel_handshake_seconds));
-    setr = res->Set(ctx, to_string(isolate, "channel_heartbeat_minutes"), Number::New(isolate, setts.channel_heartbeat_minutes));
-    setr = res->Set(ctx, to_string(isolate, "channel_inactivity_minutes"), Number::New(isolate, setts.channel_inactivity_minutes));
-    setr = res->Set(ctx, to_string(isolate, "channel_expiration_minutes"), Number::New(isolate, setts.channel_expiration_minutes));
-    setr = res->Set(ctx, to_string(isolate, "channel_germination_seconds"), Number::New(isolate, setts.channel_germination_seconds));
-    setr = res->Set(ctx, to_string(isolate, "host_pool_capacity"), Number::New(isolate, setts.host_pool_capacity));
+    setr = res->Set(ctx, to_string(isolate, "inboundPort"), Number::New(isolate, setts.inbound_port));
+    setr = res->Set(ctx, to_string(isolate, "inboundConnections"), Number::New(isolate, setts.inbound_connections));
+    setr = res->Set(ctx, to_string(isolate, "outboundConnections"), Number::New(isolate, setts.outbound_connections));
+    setr = res->Set(ctx, to_string(isolate, "manualAttemptLimit"), Number::New(isolate, setts.manual_attempt_limit));
+    setr = res->Set(ctx, to_string(isolate, "connectBatchSize"), Number::New(isolate, setts.connect_batch_size));
+    setr = res->Set(ctx, to_string(isolate, "connectTimeoutSeconds"), Number::New(isolate, setts.connect_timeout_seconds));
+    setr = res->Set(ctx, to_string(isolate, "channelHandshakeSeconds"), Number::New(isolate, setts.channel_handshake_seconds));
+    setr = res->Set(ctx, to_string(isolate, "channelHeartbeatMinutes"), Number::New(isolate, setts.channel_heartbeat_minutes));
+    setr = res->Set(ctx, to_string(isolate, "channelInactivityMinutes"), Number::New(isolate, setts.channel_inactivity_minutes));
+    setr = res->Set(ctx, to_string(isolate, "channelExpirationMinutes"), Number::New(isolate, setts.channel_expiration_minutes));
+    setr = res->Set(ctx, to_string(isolate, "channelGerminationSeconds"), Number::New(isolate, setts.channel_germination_seconds));
+    setr = res->Set(ctx, to_string(isolate, "hostPoolCapacity"), Number::New(isolate, setts.host_pool_capacity));
 
-    setr = res->Set(ctx, to_string(isolate, "hosts_file"), to_string(isolate, setts.hosts_file));
+    setr = res->Set(ctx, to_string(isolate, "hostsFile"), to_string(isolate, setts.hosts_file));
     setr = res->Set(ctx, to_string(isolate, "self"), config_authority_to_js(isolate, setts.self));
     setr = res->Set(ctx, to_string(isolate, "blacklist"), config_authorities_to_js(isolate, setts.blacklists, setts.blacklist_count));
     setr = res->Set(ctx, to_string(isolate, "peers"), config_endpoints_to_js(isolate, setts.peers, setts.peer_count));
     setr = res->Set(ctx, to_string(isolate, "seeds"), config_endpoints_to_js(isolate, setts.seeds, setts.seed_count));
 
-    setr = res->Set(ctx, to_string(isolate, "debug_file"), to_string(isolate, setts.debug_file));
-    setr = res->Set(ctx, to_string(isolate, "error_file"), to_string(isolate, setts.error_file));
-    setr = res->Set(ctx, to_string(isolate, "archive_directory"), to_string(isolate, setts.archive_directory));
+    setr = res->Set(ctx, to_string(isolate, "debugFile"), to_string(isolate, setts.debug_file));
+    setr = res->Set(ctx, to_string(isolate, "errorFile"), to_string(isolate, setts.error_file));
+    setr = res->Set(ctx, to_string(isolate, "archiveDirectory"), to_string(isolate, setts.archive_directory));
 
-    setr = res->Set(ctx, to_string(isolate, "rotation_size"), Number::New(isolate, setts.rotation_size));
-    setr = res->Set(ctx, to_string(isolate, "minimum_free_space"), Number::New(isolate, setts.minimum_free_space));
-    setr = res->Set(ctx, to_string(isolate, "maximum_archive_size"), Number::New(isolate, setts.maximum_archive_size));
-    setr = res->Set(ctx, to_string(isolate, "maximum_archive_files"), Number::New(isolate, setts.maximum_archive_files));
+    setr = res->Set(ctx, to_string(isolate, "rotationSize"), Number::New(isolate, setts.rotation_size));
+    setr = res->Set(ctx, to_string(isolate, "minimumFreeSpace"), Number::New(isolate, setts.minimum_free_space));
+    setr = res->Set(ctx, to_string(isolate, "maximumArchiveSize"), Number::New(isolate, setts.maximum_archive_size));
+    setr = res->Set(ctx, to_string(isolate, "maximumArchiveFiles"), Number::New(isolate, setts.maximum_archive_files));
 
-    setr = res->Set(ctx, to_string(isolate, "statistics_server"), config_authority_to_js(isolate, setts.statistics_server));
+    setr = res->Set(ctx, to_string(isolate, "statisticsServer"), config_authority_to_js(isolate, setts.statistics_server));
 
     setr = res->Set(ctx, to_string(isolate, "verbose"), Boolean::New(isolate, setts.verbose != 0));
-    setr = res->Set(ctx, to_string(isolate, "use_ipv6"), Boolean::New(isolate, setts.use_ipv6 != 0));
+    setr = res->Set(ctx, to_string(isolate, "useIpv6"), Boolean::New(isolate, setts.use_ipv6 != 0));
 
-    setr = res->Set(ctx, to_string(isolate, "user_agent_blacklist"), config_strings_to_js(isolate, setts.user_agent_blacklist, setts.user_agent_blacklist_count));
+    setr = res->Set(ctx, to_string(isolate, "userAgentBlacklist"), config_strings_to_js(isolate, setts.user_agent_blacklist, setts.user_agent_blacklist_count));
 
     return res;
 }
-
 
 void config_network_settings_default(v8::FunctionCallbackInfo<v8::Value> const& args) {
     Isolate* isolate = args.GetIsolate();
