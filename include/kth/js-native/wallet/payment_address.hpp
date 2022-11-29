658e0e10c88b959767e1cfc94c5834e4215da73c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-11-29 14:15:32 +0100

    remove dead code

--- include/kth/js-native/wallet/payment_address.hpp
+++ include/kth/js-native/wallet/payment_address.hpp
@@ -18,16 +18,6 @@ void wallet_payment_address_version(v8::FunctionCallbackInfo<v8::Value> const& a
 void wallet_payment_address_is_valid(v8::FunctionCallbackInfo<v8::Value> const& args);
 void wallet_payment_address_destruct(v8::FunctionCallbackInfo<v8::Value> const& args);
 
-// void kth_wallet_payment_address_set_cashaddr_prefix(char const* prefix);
-// char* kth_wallet_payment_address_encoded(kth_payment_address_t payment_address);
-// char* kth_wallet_payment_address_encoded_cashaddr(kth_payment_address_t payment_address);
-// kth_payment_address_t kth_wallet_payment_address_construct_from_string(char const* address);
-// kth_shorthash_t kth_wallet_payment_address_hash(kth_payment_address_t payment_address);
-// uint8_t kth_wallet_payment_address_version(kth_payment_address_t payment_address);
-// kth_bool_t kth_wallet_payment_address_is_valid(kth_payment_address_t payment_address);
-// void kth_wallet_payment_address_destruct(kth_payment_address_t payment_address);
-
-
 }  // namespace kth::js_native
 
 #endif //KTH_JS_WALLET_PAYMENT_ADDRESS_H_
