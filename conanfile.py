5e2e1e42e95ec86d9e882ee4a1d40e801fda611b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-05-31 18:08:16 +0200

    Node Information API (#24)

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class KnuthJsNative(ConanFile):
     # TODO(fernando): use Shared=False as default
 
     # generators = "cmake"
-    requires = (("c-api/0.35.0"))
+    requires = (("c-api/0.36.0"))
 
     def configure(self):
         self.options["c-api/*"].db = "full"
